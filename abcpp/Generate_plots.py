4e2630cb39c36e1e1b03b341a71719341f6d954e
Liang Xu <xl0418@gmail.com>
2019-11-07 15:24:08 +0100

    Generating plots function done

---
+++ abcpp/Generate_plots.py
@@ -0,0 +1,102 @@
+def generate_plots(result,plots_dir):
+    import numpy as np
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import matplotlib
+    matplotlib.use('Agg')
+    est_data = np.load(result,allow_pickle=True).item()
+    population = int(len(est_data['model_data'][0]) / 3)
+    fitness = est_data['fitness'][-2]
+
+    q5_TVP = np.argsort(fitness[:population])[-int(population // 200 + 1)]  # best 5%
+    q5_TV = np.argsort(fitness[population:2 * population])[
+                -int(population // 200 + 1)] + population  # best 5%
+    q5_TVM = np.argsort(fitness[2 * population:])[-int(population // 200 + 1)] + 2 * population  #
+    # best 5%
+
+    fit_index_TVP = np.where(fitness[:population] > fitness[q5_TVP])[0]
+    fit_index_TV = np.where(fitness[population:2 * population] > fitness[q5_TV])[0] + population
+    fit_index_TVM = np.where(fitness[2 * population:] > fitness[q5_TVM])[0] + 2 * population
+
+    previous_bestfitted_index_TVP = fit_index_TVP
+    previous_bestfitted_index_TV = fit_index_TV - population
+    previous_bestfitted_index_TVM = fit_index_TVM - 2 * population
+
+    top_pop = [len(previous_bestfitted_index_TVP),len(previous_bestfitted_index_TV),
+               len(previous_bestfitted_index_TVM)]
+
+    gamma_vec_TVP = est_data['gamma_data_TVP'][-2, previous_bestfitted_index_TVP]
+    a_vec_TVP = est_data['a_data_TVP'][-2, previous_bestfitted_index_TVP]
+    nu_vec_TVP = est_data['nu_data_TVP'][-2, previous_bestfitted_index_TVP]
+    vm_vec_TVP = est_data['vm_data_TVP'][-2, previous_bestfitted_index_TVP]
+    theta_vec_TVP = est_data['theta_data_TVP'][-2, previous_bestfitted_index_TVP]
+
+    gamma_vec_TV = est_data['gamma_data_TV'][-2, previous_bestfitted_index_TV]
+    a_vec_TV = est_data['a_data_TV'][-2, previous_bestfitted_index_TV]
+    nu_vec_TV = est_data['nu_data_TV'][-2, previous_bestfitted_index_TV]
+    vm_vec_TV = est_data['vm_data_TV'][-2, previous_bestfitted_index_TV]
+    theta_vec_TV = est_data['theta_data_TV'][-2, previous_bestfitted_index_TV]
+
+    gamma_vec_TVM = est_data['gamma_data_TVM'][-2, previous_bestfitted_index_TVM]
+    a_vec_TVM = est_data['a_data_TVM'][-2, previous_bestfitted_index_TVM]
+    nu_vec_TVM = est_data['nu_data_TVM'][-2, previous_bestfitted_index_TVM]
+    vm_vec_TVM = est_data['vm_data_TVM'][-2, previous_bestfitted_index_TVM]
+    theta_vec_TVM = est_data['theta_data_TVM'][-2, previous_bestfitted_index_TVM]
+
+    print('TVP 5th gamma = %.3e  a = %.3e nu = %.3e Vm = %f theta = %f' % (
+        np.mean(gamma_vec_TVP) ,
+        np.mean(a_vec_TVP) , np.mean(nu_vec_TVP) ,
+        np.mean(vm_vec_TVP), np.mean(theta_vec_TVP)))
+    print('Var: gamma = %.3e  a = %.3e nu = %.3e Vm = %f theta = %f' % (
+        np.var(gamma_vec_TVP) ,
+        np.var(a_vec_TVP) , np.var(nu_vec_TVP) ,
+        np.var(vm_vec_TVP), np.var(theta_vec_TVP)))
+
+    print('TV 5th gamma = %.3e  a = %.3e nu = %.3e Vm = %f theta = %f' % (
+        np.mean(gamma_vec_TV) ,
+        np.mean(a_vec_TV) , np.mean(nu_vec_TV),
+        np.mean(vm_vec_TV), np.mean(theta_vec_TV)))
+    print('Var: gamma = %.3e  a = %.3e nu = %.3e Vm = %f theta = %f' % (
+        np.var(gamma_vec_TV) ,
+        np.var(a_vec_TV) , np.var(nu_vec_TV) ,
+        np.var(vm_vec_TV), np.var(theta_vec_TV)))
+
+    print('TVM 5th gamma = %.3e  a = %.3e nu = %.3e Vm = %f theta = %f' % (
+        np.mean(gamma_vec_TVM) ,
+        np.mean(a_vec_TVM) , np.mean(nu_vec_TVM) ,
+        np.mean(vm_vec_TVM), np.mean(theta_vec_TVM)))
+    print('Var: gamma = %.3e  a = %.3e nu = %.3e Vm = %f theta = %f' % (
+        np.var(gamma_vec_TVM),
+        np.var(a_vec_TVM) , np.var(nu_vec_TVM) ,
+        np.var(vm_vec_TVM), np.var(theta_vec_TVM)))
+
+    est_para = ['gamma', 'alpha', 'nu', 'vm', 'theta']  # ,'vm'
+    model_para = ['AWC', 'UWC', 'MWC']
+    est_array = np.concatenate([gamma_vec_TVP, a_vec_TVP, nu_vec_TVP, vm_vec_TVP, theta_vec_TVP,
+                                gamma_vec_TV, a_vec_TV, nu_vec_TV, vm_vec_TV, theta_vec_TV,
+                                gamma_vec_TVM, a_vec_TVM, nu_vec_TVM, vm_vec_TVM, theta_vec_TVM])
+    est_label = np.repeat(np.tile(est_para,3),np.tile(top_pop,5))
+    model_label = np.repeat(model_para, np.array(top_pop) * len(est_para))
+
+    ss_list = {'est': est_array, 'est_label': est_label,
+               'model_label': model_label}
+    ss_df = pd.DataFrame(ss_list)
+    # plt.ioff()
+    vioplot = sns.catplot(x="model_label", y="est", col="est_label",palette=["#CB1B45","#FAD689","#0D5661"],
+                          data=ss_df, kind="box", height=5, aspect=.6, sharey=False)
+
+    vioplot.set_axis_labels("", "Estimate value")
+    # vioplot._legend.set_title('$h^2$')
+    axes = vioplot.axes.flatten()
+    axes[0].set_title("$\gamma $")
+    axes[1].set_title("$\\alpha $")
+    axes[2].set_title("$\\nu $")
+    axes[3].set_title("$V_m $")
+    axes[4].set_title("$\\theta $")
+
+    for no_plots in range(0,4):
+        axes[no_plots].ticklabel_format(style='sci', axis='y', scilimits=(0,0),useMathText=True)
+
+    vioplot.savefig(plots_dir+'/ms_plots.png')
+
--- abcpp/Switch_gui.py
+++ abcpp/Switch_gui.py
@@ -7,12 +7,14 @@ import threading
 import shutil
 import os
 import time
+from PIL import Image
+
 
 sys.path.append('C:/Liang/Trait_pop_model_sim/abcpp')
 # from sim_argu_test import simtest
 from Trait_simulator import trait_simulator
 from Continue_MS_function import Continue_trait_simulator
-
+from Generate_plots import generate_plots
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -72,7 +74,8 @@ class ParaInf(tk.Frame):
         # Frame 1
         self.labelFrame1 = tk.LabelFrame(self, text="Input and Output")
         self.labelFrame1.place(x = 15, y = 75, width=300, height=125)
-        self.browsebutton = tk.Button(self.labelFrame1, text="Browse A File", command=self.fileDialog)
+        self.browsebutton = tk.Button(self.labelFrame1, text="Browse A Dir",
+                                      command=self.fileDialog)
         self.browsebutton.place(x = 150, y = 5, width=140, height=20)
         self.data = tk.Label(self.labelFrame1, text='Data directory').place(x = 5, y = 5,
                                                                            width=140, height=20)
@@ -107,7 +110,7 @@ class ParaInf(tk.Frame):
 
         # Frame 3
         # choose the number of threads
-        self.labelFrame3 = tk.LabelFrame(self, text="Threads settings")
+        self.labelFrame3 = tk.LabelFrame(self, text="Threads setting")
         self.labelFrame3.place(x = 325, y = 200, width=170, height=50)
         default_threads = tk.DoubleVar(value=1)  # default value for the threads
         self.thr = tk.Label(self.labelFrame3, text='Threads').place(x = 5, y = 5, width=50,
@@ -273,7 +276,8 @@ class ConParaInf(tk.Frame):
         # Frame 1
         self.labelFrame1 = tk.LabelFrame(self, text="Input and Output")
         self.labelFrame1.place(x = 15, y = 75, width=300, height=125)
-        self.browsebutton = tk.Button(self.labelFrame1, text="Browse A File", command=self.fileDialog)
+        self.browsebutton = tk.Button(self.labelFrame1, text="Browse A Dir",
+                                      command=self.fileDialog)
         self.browsebutton.place(x=150, y=5, width=140, height=20)
         self.data = tk.Label(self.labelFrame1, text='Data directory').place(x=5, y=5,
                                                                             width=140, height=20)
@@ -451,13 +455,14 @@ class GeneCluScri(tk.Frame):
         # Frame1
         self.labelFrame1 = tk.LabelFrame(self, text="Input and Output")
         self.labelFrame1.place(x = 15, y = 75, width=300, height=125)
-        self.browsebutton = tk.Button(self.labelFrame1, text="Browse", command=self.fileDialog)
+        self.browsebutton = tk.Button(self.labelFrame1, text="Browse A Dir",
+                                      command=self.fileDialog)
         self.browsebutton.place(x = 150, y = 5, width=140, height=20)
         self.data = tk.Label(self.labelFrame1, text='Data directory').place(x = 5, y = 5,
                                                                            width=140, height=20)
         self.output = tk.Label(self.labelFrame1, text='Output directory').place(x = 5, y = 55,
                                                                            width=140, height=20)
-        self.browsebutton2 = tk.Button(self.labelFrame1, text="Browse",
+        self.browsebutton2 = tk.Button(self.labelFrame1, text="Browse A Dir",
                                        command=self.savedir)
         self.browsebutton2.place(x = 150, y = 55, width=140, height=20)
 
@@ -474,7 +479,8 @@ class GeneCluScri(tk.Frame):
         self.iterations.place(x=150, y=5, width=140, height=20)
         self.iterations.bind('<Return>', lambda event:self.update_iterations())
         default_particles = tk.DoubleVar(value=1000)  # default value for the particles
-        self.par = tk.Label(self.labelFrame2, text='Particles').grid(row=3, sticky="W")
+        self.par = tk.Label(self.labelFrame2, text='Particles').place(x = 5, y = 30, width=140,
+                                                                     height=20)
         self.particles = tk.Spinbox(self.labelFrame2, from_=100, to=100000,
                                  command=self.update_particles, textvariable=default_particles)
         self.particles.place(x=150, y=30, width=140, height=20)
@@ -592,6 +598,10 @@ class GeneCluScri(tk.Frame):
     def copy_algorithm_script(self):
         current_algorithm_script = os.getcwd()+"\\abcpp\\Trait_simulator_cluster.py"
         shutil.copy2(current_algorithm_script,self.save_dir)
+        piccompute_script = os.getcwd()+"\\abcpp\\pic_compute.py"
+        shutil.copy2(piccompute_script,self.save_dir)
+        tpupdate_script = os.getcwd()+"\\abcpp\\tp_update_theta.py"
+        shutil.copy2(tpupdate_script,self.save_dir)
         print("Copy the algorithm script to %s" % (self.save_dir))
 
     def create_bash_script(self):
@@ -656,89 +666,35 @@ class Plots(tk.Frame):
         button3.place(x=365, y=15, width=150, height=20)
         button4.place(x=515, y=15, width=100, height=20)
 
-        label = tk.Label(self, text="Generate Cluster Scripts", font=controller.title_font)
-        label.place(x=15, y=45, width=400, height=20)
+        label = tk.Label(self, text="Generate plots", font=controller.title_font)
+        label.place(x=15, y=45, width=200, height=20)
 
         # Frame1
-        self.labelFrame1 = tk.LabelFrame(self, text="Input and Output")
+        self.labelFrame1 = tk.LabelFrame(self, text="Plot setting")
         self.labelFrame1.place(x=15, y=75, width=300, height=125)
-        self.browsebutton = tk.Button(self.labelFrame1, text="Browse", command=self.fileDialog)
+        self.browsebutton = tk.Button(self.labelFrame1, text="Browse A File",
+                                      command=self.fileDialog)
         self.browsebutton.place(x=150, y=5, width=140, height=20)
-        self.data = tk.Label(self.labelFrame1, text='Data directory').place(x=5, y=5,
+        self.data = tk.Label(self.labelFrame1, text='Result directory').place(x=5, y=5,
                                                                             width=140, height=20)
-        self.output = tk.Label(self.labelFrame1, text='Output directory').place(x=5, y=55,
+        self.output = tk.Label(self.labelFrame1, text='Plot directory').place(x=5, y=55,
                                                                                 width=140,
                                                                                 height=20)
-        self.browsebutton2 = tk.Button(self.labelFrame1, text="Browse",
+        self.browsebutton2 = tk.Button(self.labelFrame1, text="Browse A Dir",
                                        command=self.savedir)
         self.browsebutton2.place(x=150, y=55, width=140, height=20)
 
-        # Frame 2
-        # choose the number of iterations and particles
-        self.labelFrame2 = tk.LabelFrame(self, text="Structure of the algorithm")
-        self.labelFrame2.place(x=15, y=200, width=300, height=75)
-        default_iterations = tk.DoubleVar(value=10)  # default value for the iterations
-        self.iter = tk.Label(self.labelFrame2, text='Iterations').place(x=5, y=5,
-                                                                        width=140, height=20)
-        self.iterations = tk.Spinbox(self.labelFrame2, from_=1, to=100,
-                                     command=self.update_iterations,
-                                     textvariable=default_iterations)
-        self.iterations.place(x=150, y=5, width=140, height=20)
-        self.iterations.bind('<Return>', lambda event: self.update_iterations())
-        default_particles = tk.DoubleVar(value=1000)  # default value for the particles
-        self.par = tk.Label(self.labelFrame2, text='Particles').grid(row=3, sticky="W")
-        self.particles = tk.Spinbox(self.labelFrame2, from_=100, to=100000,
-                                    command=self.update_particles, textvariable=default_particles)
-        self.particles.place(x=150, y=30, width=140, height=20)
-        self.particles.bind('<Return>', lambda event: self.update_particles())
-
-        # Frame 3
-        # choose the number of threads
-        self.labelFrame3 = tk.LabelFrame(self, text="Threads settings")
-        self.labelFrame3.place(x=325, y=200, width=170, height=50)
-        default_threads = tk.DoubleVar(value=1)  # default value for the threads
-        self.thr = tk.Label(self.labelFrame3, text='Threads').place(x=5, y=5, width=50,
-                                                                    height=20)
-        self.thread = tk.Spinbox(self.labelFrame3, from_=1, to=8, command=self.update_threads,
-                                 textvariable=default_threads)
-        self.thread.place(x=60, y=5, width=90, height=20)
-        self.thread.bind('<Return>', lambda event: self.update_threads())
-
-        # Frame 4
-        # choose one summary stats
-        self.labelFrame4 = tk.LabelFrame(self, text="Summary statistics")
-        self.labelFrame4.place(x=505, y=200, width=210, height=50)
-        self.choice = tk.IntVar()
-        self.choice.set(1)
-        self.smtdbutton = tk.Radiobutton(self.labelFrame4, text='smtd', variable=self.choice,
-                                         value=1, command=self.update_sstats).place(x=5,
-                                                                                    y=5,
-                                                                                    width=60,
-                                                                                    height=20)
-        self.umtdbutton = tk.Radiobutton(self.labelFrame4, text='umtd', variable=self.choice,
-                                         value=2,
-                                         command=self.update_sstats).place(x=70,
-                                                                           y=5,
-                                                                           width=60,
-                                                                           height=20)
-        self.picsbutton = tk.Radiobutton(self.labelFrame4, text='pics', variable=self.choice,
-                                         value=3,
-                                         command=self.update_sstats).place(x=135,
-                                                                           y=5,
-                                                                           width=60,
-                                                                           height=20)
 
         # Output info
         self.labelFrame5 = tk.LabelFrame(self, text="Report of the progress")
         self.labelFrame5.place(x=15, y=280, width=700, height=450)
+        self.progressbar = ttk.Progressbar(self.labelFrame5, mode='indeterminate',length=500)
+        self.progressbar.place(x = 5, y = 5, width=400, height=20)
         # self.report = Text(master)
         # Set default values
-        self.sstats_value = self.choice.get()
-        self.threads_value = self.thread.get()
-        self.iterations_value = self.iterations.get()
-        self.particles_value = self.particles.get()
 
-        self.gobutton = tk.Button(self.labelFrame5, text="Generate", command=self.generate_all,
+
+        self.gobutton = tk.Button(self.labelFrame5, text="Generate", command=self.threading_generateplots,
                                   height=20,
                                   width=80)
         self.gobutton.place(x=600, y=5, width=80, height=20)
@@ -749,11 +705,11 @@ class Plots(tk.Frame):
         # Open a file dialog
 
     def fileDialog(self):
-        self.treedatadir = fd.askdirectory()
+        self.resultdir = fd.askopenfilename()
         self.label = tk.Label(self.labelFrame1, text="")
         self.label.place(x=5, y=30, width=285, height=20)
-        self.label.configure(text=self.treedatadir)
-        print('Data file is in the directory: %s' % self.treedatadir)
+        self.label.configure(text=self.resultdir)
+        print('Data file is in the directory: %s' % self.resultdir)
 
     def savedir(self):
         self.save_dir = fd.askdirectory()
@@ -762,89 +718,42 @@ class Plots(tk.Frame):
         self.label.configure(text=self.save_dir)
         print('Saving dir is in the directory: %s' % self.save_dir)
 
-    def update_sstats(self):
-        self.sstats_value = self.choice.get()
-        stats_vec = ['smtd', 'umtd', 'pics']
-        stats = stats_vec[self.sstats_value - 1]
-        print('The summary stats is chosed to be: %s' % stats)
+    def generate_plots(self):
+        generate_plots(result=self.resultdir,plots_dir=self.save_dir)
 
-    def update_threads(self):
-        self.threads_value = self.thread.get()
-        print('No. of threads to be used: %s' % str(self.threads_value))
 
-    def update_iterations(self):
-        self.iterations_value = self.iterations.get()
-        print('No. of iterations to be used: %s' % str(self.iterations_value))
 
-    def update_particles(self):
-        self.particles_value = self.particles.get()
-        print('No. of particles to be used: %s' % str(self.particles_value))
+    def threading_generateplots(self):
+        self.show_progress(True)
 
-    def copy_treedata(self):
-        source_file = self.treedatadir
-        src_files = os.listdir(source_file)
-        # create treedata folder in the destination
-        des_treedata_dir = os.path.join(self.save_dir, "treedata")
-        if not os.path.exists(des_treedata_dir):
-            os.mkdir(des_treedata_dir)
-        for file_name in src_files:
-            full_file_name = os.path.join(source_file, file_name)
-            des_file_name = os.path.join(des_treedata_dir, file_name)
-            if os.path.isdir(full_file_name):
-                shutil.copytree(full_file_name, des_file_name, symlinks=False, ignore=None)
-            else:
-                shutil.copy(full_file_name, des_file_name)
-        print("Copy the data file from %s to %s" % (source_file, self.save_dir))
+        # Start thread to process file.
+        self.thread = threading.Thread(target=self.generate_plots)
+        self.thread.daemon = True # Allow the program to terminate without waiting for the thread to finish.
+        self.thread.start()
 
-    def copy_algorithm_script(self):
-        current_algorithm_script = os.getcwd() + "\\abcpp\\Trait_simulator_cluster.py"
-        shutil.copy2(current_algorithm_script, self.save_dir)
-        print("Copy the algorithm script to %s" % (self.save_dir))
+        # Start checking the thread.
+        self.process_file_check()
 
-    def create_bash_script(self):
-        stats_vec = ['smtd', 'umtd', 'pics']
-        stats = stats_vec[self.sstats_value - 1]
-        with open(self.save_dir + '\\run.sh', 'w') as rsh:
-            rsh.write('''\
-    #!/bin/bash
-    #SBATCH --time=10-00:00:00
-    #SBATCH --partition=gelifes
-    #SBATCH --ntasks=1
-    #SBATCH --nodes=1
-    #SBATCH --mem=10GB
-    #SBATCH --cpus-per-task=20
-    #SBATCH --output=MSumtd.log
-    #SBATCH --job-name=MSumtd
 
-    python3 Trait_simulator_cluster.py --treedata %s --result %s --num_threads %i --sstats %s 
-    --num_iterations %i --num_particles %i
-                    ''' % ('treedata\\', 'ParaInf_result_' + stats, int(self.threads_value), stats,
-                           int(self.iterations_value), int(self.particles_value)))
+    def process_file_check(self):
+        if self.thread.is_alive():
+            # Thread is still running, check thread again in 10 milliseconds.
+            self.after(10, self.process_file_check)
 
-    def generate_all(self):
-        self.create_bash_script()
-        self.copy_treedata()
-        self.copy_algorithm_script()
-        self.GenCluScript_log()
-        self.outputtextbox.delete('1.0', tk.END)
-        # read the data
-        with open("GenerateClusterScript_log.txt", "r") as f:
-            self.outputtextbox.insert(tk.INSERT, f.read())
-        self.outputtextbox.see(tk.END)
-        with open(self.save_dir + "/run.sh", "r") as f:
-            self.outputtextbox.insert(tk.INSERT, f.read())
-        self.outputtextbox.see(tk.END)
+        else:
+            # Thread finished, handle processed results.
+            # Do something with `self.fdata`.
+            self.show_progress(False)
 
-    def GenCluScript_log(self):
-        output_log = sys.stdout
-        f = open('GenerateClusterScript_log.txt', 'w')
-        sys.stdout = f
-        print("Copying the data from %s to %s ... \n" % (self.treedatadir, self.save_dir))
-        print("Generating run.sh script is done!")
-        print("run.sh is ...")
-        sys.stdout = output_log
-        f.close()
 
+    def show_progress(self, start):
+        if start:
+            self.progressbar.grid()
+            self.progressbar.start()
+        else:
+            self.progressbar.stop()
+            with Image.open(self.save_dir+'/ms_plots.png') as img:
+                img.show()
 
 if __name__ == "__main__":
     app = SampleApp()
